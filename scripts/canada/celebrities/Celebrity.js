cb956f9900548381ebdaf15cb4c021f1c4bcf8cc
Michael Wright <info@saymoremultimedia.com>
2021-08-25 14:57:45 -0500

    created and coded CityList.js; added Canada images

--- scripts/canada/celebrities/Celebrity.js
+++ scripts/canada/celebrities/Celebrity.js
@@ -1,9 +1,12 @@
 // This will render each celebrity object as HTML
 
 export const Celebrity = (celebrityObj) => {
-    return `<li class="citizen__name"><h4></h4></li>
-    <li class="citizen__occupation"></li>
-    <li class="citizen__description"></li>
-    <li class="citizen__website"></li>
-    <hr>`
+    return `<article class="citizen__block">
+    <ul class="citizen__details"> 
+        <li class="citizen__name"><h4>${celebrityObj.name}</h4></li>
+        <li class="citizen__occupation">Occupation: ${celebrityObj.occupation}</li>
+        <li class="citizen__description">${celebrityObj.description}</li>
+        <li class="citizen__website"><a href="${celebrityObj.website}" target="_blank">Visit Website</a></li>
+    </ul>
+</article>`
 }
---
+++ scripts/canada/cities/CityList.js
@@ -0,0 +1,27 @@
+/**
+ *  CityList renders individual city objects as HTML
+ */
+
+import { getCity } from "./CityDataProvider.js";
+import { City } from "./City.js";
+
+export const CityList = () => {
+    const getCities = getCity();
+    addCityToDOM(getCities);
+}
+
+export const addCityToDOM = (arrayOfCities) => {
+    
+    // Get a reference to the <article class="country__detail-block"> element
+    const contentElement = document.querySelector(".country__detail-block");
+
+    // Add to the existing HTML in the content element contentElement
+    let cityHTMLRepresentation = "";
+    for (const oneCity of arrayOfCities) {
+        cityHTMLRepresentation += City(oneCity);
+    }
+
+    // Add to the existing HTML in the content element
+    contentElement.innerHTML += ` ${cityHTMLRepresentation}`;
+
+}
