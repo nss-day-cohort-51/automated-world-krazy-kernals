4cf49b056a22a865d66196c4001bbbfa44ccd1ea
Michael Wright <info@saymoremultimedia.com>
2021-08-25 12:18:21 -0500

    coded JS data files and had Bryan review

---
+++ scripts/canada/celebrities/Celebrity.js
@@ -0,0 +1,9 @@
+// This will render each celebrity object as HTML
+
+export const Celebrity = (celebrityObj) => {
+    return `<li class="citizen__name"><h4></h4></li>
+    <li class="citizen__occupation"></li>
+    <li class="citizen__description"></li>
+    <li class="citizen__website"></li>
+    <hr>`
+}
---
+++ scripts/canada/cities/CityDataProvider.js
@@ -0,0 +1,28 @@
+// This is the data array of cities
+
+export const getCity = () => {
+    return cityCollection;
+  };
+
+const cityCollection = [
+  {
+      name: "Vancouver",
+      population: 675218,
+      description: "A lot of movies are made in Vancouver especially movies that take place in New York.",
+      website: "https://vancouver.ca/",
+  },
+  {
+    name: "Ottawa",
+    population: 1300000,
+    description: "Canada's capital city that sounds like it's pronounced backwards.",
+    website: "https://ottawa.ca/en",
+  },
+  {
+    name: "Montreal",
+    population: 1780000,
+    description: "Montreal is set on an island in the Saint Lawrence River and named after Mt. Royal, the triple-peaked hill inside the city.",
+    website: "https://montreal.ca/en/",
+  },
+  
+];
+
