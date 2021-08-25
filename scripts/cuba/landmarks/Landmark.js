d458583324a0ecbf3dc4d4307ea460ed55f6e5f0
LD Bartholomew <LDBartholomew@yahoo.com>
2021-08-25 16:02:06 -0500

    modified cuba.html js cities

---
+++ scripts/cuba/landmarks/Landmark.js
@@ -0,0 +1,13 @@
+
+
+export const Landmark = (landmarkObj)=> {
+    return`<article class="landmark__block>"
+    <ul class="landmark__details">
+        <li class="landmark__name"><h4>${landmarkObj.name}</h4></li>
+         <li class="landmark__year-created">Created: ${landmarkObj.yearCreated}</li>
+        <li class="landmark__description">${landmarkObj.description}</li>
+        <li class="landmark__website"><a href="${landmarkObj.website}" target="_blank">Visit Website</a></li>
+    </ul>
+</article>`
+
+}
---
+++ scripts/cuba/landmarks/LandmarkDataProvider.js
@@ -0,0 +1,25 @@
+export const getCity = ()=> {
+    return cityCollection;
+};
+
+const landmarkCollection = [
+
+    {
+        name: "Museum of the Revolution",
+        yearCreated: 1920,
+        description: "History of War",
+        website: "https://en.wikipedia.org/wiki/Museum_of_the_Revolution_(Cuba)",
+    }, 
+    {
+        name: "Che Guevara Mausoleum",
+        yearCreated: 1997,
+        description: "Revolutionary's Final Resting Place",
+        website: "https://en.wikipedia.org/wiki/Che_Guevara_Mausoleum",
+    },
+    {
+        name: "El Morro Fortress",
+        yearCreated: 1539,
+        description: "Protection Against Pirates",
+        website: "https://en.wikipedia.org/wiki/Castillo_San_Felipe_del_Morro",
+    },
+];
