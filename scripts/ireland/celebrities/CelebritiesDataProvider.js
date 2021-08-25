372ede2c386bcd52ad64d2afa81c87d3237dc86d
Kaitlyn Long <katielong11@gmail.com>
2021-08-25 16:00:18 -0500

    created and coded js for ireland

---
+++ scripts/ireland/celebrities/CelebritiesDataProvider.js
@@ -0,0 +1,29 @@
+// data for ireland celebrities
+
+export const getCelebrity = () => {
+    return celebrityCollection;
+};
+
+const celebrityCollection = [
+    {
+    name: "Liam Neeson",
+    occupation: "actor",
+    description: "Liam Neeson is an Irish born citizen turned American actor playing many roles in action films",
+    website: "https://www.imdb.com/name/nm0000553/"
+    },
+
+    {
+    name: "Ben Stiller",
+    occupation: "actor",
+    description: "Ben Stiller is an Irish born citizen turned American actor playing many roles in rom-com films",
+    website: "https://www.imdb.com/name/nm0001774/"
+    },
+
+    {
+    name: "Jack Nicholson",
+    occupation: "actor",
+    description: "Jack Nicholson is an Irish born citizen turned American actor playing many roles in action/rom-com films",
+    website: "https://www.imdb.com/name/nm0000197/"
+    },
+    
+]
