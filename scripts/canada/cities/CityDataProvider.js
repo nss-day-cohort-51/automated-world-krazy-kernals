6d25e20a481dad17bc11b5febcdd557d756113ca
Tia Thomason <88596394+TT-615@users.noreply.github.com>
2021-08-26 11:54:52 -0500

    update to mexico and add js files

---
+++ scripts/canada/cities/CityDataProvider.js
@@ -0,0 +1,29 @@
+// This is the data array of cities
+
+export const getCity= () => {
+    return cityColection;
+}:
+
+const cityCollection = [
+    {
+        name: "Mexico City",
+        population: 0,
+        description:"jjjdafjl",
+        website: "wwwajekfj"
+
+    },
+    {
+        name: "Mexico City"
+        population: 
+        description:
+        website:
+        
+    },
+    {
+        name: "Mexico City"
+        population:
+        description:
+        website:
+        
+    },
+];
---
+++ scripts/mexico/landmarks/LandmarkDataProvider.js
@@ -0,0 +1,28 @@
+// This is the data array of landmarks
+
+export const getLandmark = () => {
+    return landmarkCollection;
+};
+
+const landmarkCollection = [
+    {
+        name:"Art Museum :Museo Nacional de Arte",
+        yearCreated: "1982",
+        description: "Center for Art and Education Programs",
+        website: "http://www.munal.mx/en",
+    },
+    {
+        
+        name:"Gracias a Dios. 2010 Hip, stylish mezcal distillery",
+        yearCreated: "2010",
+        description: "Hip, stylish mezcal distillery",
+        website: "  https://www.thankgad.com/        ",
+    },
+    {
+        
+        name:"Papaya Playa Project",
+        yearCreated: "2011",
+        description: "World renowned jungle parties, and music culture hub.",
+        website: "https://www.papayaplayaproject.com/        ",
+    },
+]
